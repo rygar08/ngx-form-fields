3a0e25c984e0bdc0d4702731c45f7df05d352f65
Ryan Layton <ryanlayton08@gmail.com>
2020-12-05 12:33:01 +1000

    Removed .map.ts file generation

---
+++ projects/ngx-form-fields/src/public-api.ts
@@ -0,0 +1,7 @@
+
+
+export * from './lib/field.component';
+export * from './lib/form.component';
+export * from './lib/fieldBase.component';
+export * from './lib/fieldGroup.component';
+export * from './lib/formFields.module';
